83341d26305a81105e6c83bbeaa37b017fb068b6
jollypuppet <galella.andrea@gmail.com>
2020-06-30 23:43:32 +0200

    Implemented switch theme in the menu drawer

--- app/src/main/java/com/pizzaboys/randompizzaapp/MainActivity.java
+++ app/src/main/java/com/pizzaboys/randompizzaapp/MainActivity.java
@@ -3,12 +3,15 @@ package com.pizzaboys.randompizzaapp;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 import androidx.appcompat.widget.Toolbar;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.CompoundButton;
 import android.widget.Switch;
+import android.widget.Toast;
 
 import com.google.android.material.navigation.NavigationView;
 
@@ -18,6 +21,7 @@ import java.util.HashSet;
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private DrawerLayout drawer;
+    private Switch switch_theme;
 
     private Collection<Ingredient> ingredients = new HashSet<>();
     private Collection<Pizza> pizzas = new HashSet<>();
@@ -41,12 +45,39 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         drawer.addDrawerListener(toggle);
         toggle.syncState();
 
+        //SWITCH_THEME
+        MenuItem menuItem = navigationView.getMenu().findItem(R.id.app_bar_switch);
+
+        switch_theme = menuItem.getActionView().findViewById(R.id.drawer_switch);
+
+            //SEE IF THERE IS NIGHT MODE BY DEFAULT
+        if (AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_UNSPECIFIED) {
+            switch_theme.setChecked(true);
+        }
+
+            //SET LISTENER
+        switch_theme.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if (b) {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                } else {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                }
+
+                Toast.makeText(getApplicationContext(), "Theme changed - " +
+                                (b ? switch_theme.getTextOn().toString() : switch_theme.getTextOff().toString()),
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+        //END SWITCH_THEME
+
         createIngredientsSet();
         createPizzaSet();
 
     }
 
-    public void createIngredientsSet(){
+    public void createIngredientsSet() {
         Ingredient patateLesse = new Ingredient("Patate lesse");
         ingredients.add(patateLesse);
         Ingredient patatineFritte = new Ingredient("Patatine fritte");
@@ -55,7 +86,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         ingredients.add(pomodoro);
         Ingredient funghi = new Ingredient("Funghi");
         ingredients.add(funghi);
-        Ingredient pesto = new Ingredient("Pesto","Salse");
+        Ingredient pesto = new Ingredient("Pesto", "Salse");
         ingredients.add(pesto);
         Ingredient peperoni = new Ingredient("Peperoni");
         ingredients.add(peperoni);
@@ -63,17 +94,17 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         ingredients.add(salsiccia);
         Ingredient wurstel = new Ingredient("Wurstel");
         ingredients.add(wurstel);
-        Ingredient olive = new Ingredient("Olive","Verdure");
+        Ingredient olive = new Ingredient("Olive", "Verdure");
         ingredients.add(olive);
         Ingredient tonno = new Ingredient("Tonno");
         ingredients.add(tonno);
-        Ingredient zucchine = new Ingredient("Zucchine","Verdure");
+        Ingredient zucchine = new Ingredient("Zucchine", "Verdure");
         ingredients.add(zucchine);
-        Ingredient panna = new Ingredient("Panna","Salse");
+        Ingredient panna = new Ingredient("Panna", "Salse");
         ingredients.add(panna);
     }
 
-    public void createPizzaSet(){
+    public void createPizzaSet() {
         Pizza margherita = new Pizza("Margherita");
         pizzas.add(margherita);
         Pizza marinara = new Pizza("Marinara");
@@ -87,7 +118,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
 
-        switch(item.getItemId()){
+        switch (item.getItemId()) {
             //CREATE FRAGMENTS
         }
 
