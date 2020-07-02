70f7d2940d4f791272d7ab8de5473c8732c43b4a
jollypuppet <galella.andrea@gmail.com>
2020-07-02 16:48:04 +0200

    Added Fragment Good Class and Layout

--- app/src/main/java/com/pizzaboys/randompizzaapp/PGoodFragment.java
+++ app/src/main/java/com/pizzaboys/randompizzaapp/PGoodFragment.java
@@ -9,11 +9,11 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-public class P360Fragment extends Fragment {
+public class PGoodFragment extends Fragment {
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_360, container, false);
+        return inflater.inflate(R.layout.fragment_good, container, false);
     }
 }
