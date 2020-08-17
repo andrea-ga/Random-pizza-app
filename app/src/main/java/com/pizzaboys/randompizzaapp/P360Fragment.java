5e877f411abe24923016c14a785cf27d1a3a0663
jollypuppet <galella.andrea@gmail.com>
2020-08-17 16:10:45 +0200

    Implemented test adView banners

--- app/src/main/java/com/pizzaboys/randompizzaapp/P360Fragment.java
+++ app/src/main/java/com/pizzaboys/randompizzaapp/P360Fragment.java
@@ -9,11 +9,24 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
+
 public class P360Fragment extends Fragment {
+
+    private AdView mAdView;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_360, container, false);
+        View v = inflater.inflate(R.layout.fragment_360, container, false);
+
+        MobileAds.initialize(getActivity(), "ca-app-pub-3940256099942544~3347511713");
+        mAdView = v.findViewById(R.id.adView3);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
+
+        return v;
     }
 }
