624d3244db7dfbff72c6a9292ccc79fd1b0b3e9c
jollypuppet <galella.andrea@gmail.com>
2020-08-22 19:17:05 +0200

    Deleted test adView banners and implementation

--- app/src/main/java/com/pizzaboys/randompizzaapp/PGoodFragment.java
+++ app/src/main/java/com/pizzaboys/randompizzaapp/PGoodFragment.java
@@ -9,23 +9,12 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-import com.google.android.gms.ads.AdRequest;
-import com.google.android.gms.ads.AdView;
-import com.google.android.gms.ads.MobileAds;
-
 public class PGoodFragment extends Fragment {
-
-    private AdView mAdView;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_good, container, false);
-
-        MobileAds.initialize(getActivity(), "ca-app-pub-3940256099942544~3347511713");
-        mAdView = v.findViewById(R.id.adView2);
-        AdRequest adRequest = new AdRequest.Builder().build();
-        mAdView.loadAd(adRequest);
 
         return v;
     }
